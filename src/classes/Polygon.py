6e0df87dd8dee7417c3aa71155795462d431cc5b
Thiago Gadelha <thiagogadelhaa1@gmail.com>
2023-05-01 09:28:18 -0300

    Usando Numpy para a criação do Polygon

--- src/classes/Polygon.py
+++ src/classes/Polygon.py
@@ -1,24 +1,24 @@
+import numpy as np
+
 class Polygon:
     
-    def __init__(self,matrix=None):
+    def __init__(self, matrix=None):
         if matrix is None:
-            self.points =[]
+            self.points = np.empty((0,2), int)
         else:
-            self.points = matrix
+            self.points = np.array(matrix)
 
     @property
     def points(self):
         return self._points
-    
+
     @points.setter
-    def points(self,matrix):
-        
-        self._points=matrix
+    def points(self, matrix):
+        self._points = np.array(matrix)
 
-    def addPoint(self, x,y):
+    def addPoint(self, x, y):
+        point = np.array([[x,y]])
+        self._points =  np.concatenate([self._points, point], axis=0)
 
-        self._points= [*self.points,[x,y]]
-
-    def addPoints(self,matrix):
-
-        self._points = [*self.points, *matrix]
+    def addPoints(self, matrix):
+        self._points = np.concatenate([self._points, matrix], axis=0)
