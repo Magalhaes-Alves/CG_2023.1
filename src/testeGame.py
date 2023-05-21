1f80126ffe6f3c129208ae9d0b0af85eb47ec625
Thiago Gadelha <thiagogadelhaa1@gmail.com>
2023-05-21 16:13:19 -0300

    Estrutura definida e inicio do game

--- src/testeGame.py
+++ src/testeGame.py
@@ -7,46 +7,44 @@ import numpy as np
 from classes.Polygon import Polygon
 
 
-p = Nave(250,250,10)
-
-
 def game():
     window = Window(800,600)
     janela = Window(600,600)
 
-    janela_teorica = [100,100,300,300]
+    janela_teorica = [300,300,500,500]
 
-    asteroids=[]
-    bullets=[]
+    nave_real = Nave(window.width//2,window.height//2,10)
+
+    asteroids_real = []
+    asteroids_janela = []
+    bullets_real = []
+    bullets_janela = []
     naves =[]
     
     while(True):
-        #janela.screen.fill((0,0,0))
+        janela.screen.fill((0,0,0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    print("Comando: Cima")
-                elif event.key == pygame.K_DOWN:
-                    print("Comando: Baixo")
-                elif event.key == pygame.K_LEFT:
-                    print("Comando: Esquerda")
+                if event.key == pygame.K_LEFT:
+                    nave_real.rotacionar(False,window.width,window.height)
                 elif event.key == pygame.K_RIGHT:
-                    p.rotacionar(True,800,600)
+                    nave_real.rotacionar(True,window.width,window.height)
                 elif event.key == pygame.K_SPACE:
                     print("Comando: Espaço")  
 
-        subject_polygon =p.points
-        clipping_polygon = [(100,100),(300,100),(300,300),(100,300)]
+        subject_polygon = nave_real.points
+        clipping_polygon = [(janela_teorica[0],janela_teorica[1]),(janela_teorica[2],janela_teorica[1]),
+                            (janela_teorica[2],janela_teorica[3]),(janela_teorica[0],janela_teorica[3])]
 
         #subject_polygon = np.array(subject_polygon)
         clipping_polygon = np.array(clipping_polygon)
         clipped_polygon = clip(subject_polygon, clipping_polygon)
-
-        p2 = Polygon(clipped_polygon)
-        mapWindow(p2,janela_teorica,janela)
-        p2.desenha(janela)
+        clipped_polygon = [(int(x[0]), int(x[1])) for x in clipped_polygon]
+        nave_jogo = Polygon(clipped_polygon)
+        mapWindow(nave_jogo,janela_teorica,janela)
+        nave_jogo.scanline(janela,(255,0,0,255))
         pygame.display.update()
 
 
