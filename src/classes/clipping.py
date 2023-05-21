6f34285f3836729cc0d30de0dffbebecf9bf1245
Thiago Gadelha <thiagogadelhaa1@gmail.com>
2023-05-21 14:13:19 -0300

    Clipping e exemplo de uso

--- src/classes/clipping.py
+++ src/classes/clipping.py
@@ -59,19 +59,135 @@ def cohen_sutherland(xmin, ymin, xmax, ymax, x1, y1, x2, y2):
 
     if accept:
         print("Linha recortada de ({},{}) para ({},{})".format(x1, y1, x2, y2))
+        return [(int(x1),int(y1)), (int(x2),int(y2))]
     else:
         print("Linha completamente fora da janela")
+        return None
+"""
+Exemplo de uso
 
-# Definição da viewport
+Definição da viewport
 xmin = 100
 ymin = 100
 xmax = 400
 ymax = 300
 
-#Pontos da linha
+Pontos da linha
 x1 = 50
 y1 = 150
 x2 = 450
 y2 = 250
 
-cohen_sutherland(xmin, ymin, xmax, ymax, x1, y1, x2, y2)
+cohen_sutherland(xmin, ymin, xmax, ymax, x1, y1, x2, y2)"""
+
+def is_inside(p1,p2,q):
+    R = (p2[0] - p1[0]) * (q[1] - p1[1]) - (p2[1] - p1[1]) * (q[0] - p1[0])
+    if R <= 0:
+        return True
+    else:
+        return False
+
+def compute_intersection(p1,p2,p3,p4):
+    
+    """
+    given points p1 and p2 on line L1, compute the equation of L1 in the
+    format of y = m1 * x + b1. Also, given points p3 and p4 on line L2,
+    compute the equation of L2 in the format of y = m2 * x + b2.
+    
+    To compute the point of intersection of the two lines, equate
+    the two line equations together
+    
+    m1 * x + b1 = m2 * x + b2
+    
+    and solve for x. Once x is obtained, substitute it into one of the
+    equations to obtain the value of y.
+    
+    if one of the lines is vertical, then the x-coordinate of the point of
+    intersection will be the x-coordinate of the vertical line. Note that
+    there is no need to check if both lines are vertical (parallel), since
+    this function is only called if we know that the lines intersect.
+    """
+    
+    # if first line is vertical
+    if p2[0] - p1[0] == 0:
+        x = p1[0]
+        
+        # slope and intercept of second line
+        m2 = (p4[1] - p3[1]) / (p4[0] - p3[0])
+        b2 = p3[1] - m2 * p3[0]
+        
+        # y-coordinate of intersection
+        y = m2 * x + b2
+    
+    # if second line is vertical
+    elif p4[0] - p3[0] == 0:
+        x = p3[0]
+        
+        # slope and intercept of first line
+        m1 = (p2[1] - p1[1]) / (p2[0] - p1[0])
+        b1 = p1[1] - m1 * p1[0]
+        
+        # y-coordinate of intersection
+        y = m1 * x + b1
+    
+    # if neither line is vertical
+    else:
+        m1 = (p2[1] - p1[1]) / (p2[0] - p1[0])
+        b1 = p1[1] - m1 * p1[0]
+        
+        # slope and intercept of second line
+        m2 = (p4[1] - p3[1]) / (p4[0] - p3[0])
+        b2 = p3[1] - m2 * p3[0]
+    
+        # x-coordinate of intersection
+        x = (b2 - b1) / (m1 - m2)
+    
+        # y-coordinate of intersection
+        y = m1 * x + b1
+    
+    intersection = (x,y)
+    
+    return intersection
+
+def clip(subject_polygon,clipping_polygon):
+    
+    final_polygon = subject_polygon.copy()
+    clipping_polygon = clipping_polygon[::-1]
+    for i in range(len(clipping_polygon)):
+        
+        # stores the vertices of the next iteration of the clipping procedure
+        next_polygon = final_polygon.copy()
+        
+        # stores the vertices of the final clipped polygon
+        final_polygon = []
+        
+        # these two vertices define a line segment (edge) in the clipping
+        # polygon. It is assumed that indices wrap around, such that if
+        # i = 1, then i - 1 = K.
+        c_edge_start = clipping_polygon[i - 1]
+        c_edge_end = clipping_polygon[i]
+        
+        for j in range(len(next_polygon)):
+            
+            # these two vertices define a line segment (edge) in the subject
+            # polygon
+            s_edge_start = next_polygon[j - 1]
+            s_edge_end = next_polygon[j]
+            
+            if is_inside(c_edge_start,c_edge_end,s_edge_end):
+                if not is_inside(c_edge_start,c_edge_end,s_edge_start):
+                    intersection = compute_intersection(s_edge_start,s_edge_end,c_edge_start,c_edge_end)
+                    final_polygon.append(intersection)
+                final_polygon.append(tuple(s_edge_end))
+            elif is_inside(c_edge_start,c_edge_end,s_edge_start):
+                intersection = compute_intersection(s_edge_start,s_edge_end,c_edge_start,c_edge_end)
+                final_polygon.append(intersection)
+    
+    return (final_polygon)
+
+"""Exemplo de uso
+subject_polygon = [(100, 100), (200, 100), (200, 200), (100, 200)]  # Polígono a ser recortado
+clip_polygon = [(150, 150), (250, 150), (250, 250), (150, 250)]  # Polígono de recorte
+
+result_polygon = sutherland_hodgman(subject_polygon, clip_polygon)
+print("Polígono recortado:", result_polygon)"""
--- src/viewportTest.py
+++ src/viewportTest.py
@@ -1,6 +1,7 @@
 from classes.Polygon import Polygon
 from classes.viewport import *
 from classes.Tranformations import *
+from classes.clipping import *
 from classes.Window import Window
 from time import sleep
 import pygame
@@ -34,18 +35,89 @@ def animacao():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    print("Comando: Cima")
+                elif event.key == pygame.K_DOWN:
+                    print("Comando: Baixo")
+                elif event.key == pygame.K_LEFT:
+                    print("Comando: Esquerda")
+                elif event.key == pygame.K_RIGHT:
+                    print("Comando: Direita")
+                elif event.key == pygame.K_SPACE:
+                    print("Comando: Espaço")  
     
         window.screen.fill((0,0,0))
         p.scanlineT(window)
         
-        rotacao(p,2,200,300)
+        rotacao(p,30,200,300)
         
         pygame.display.update()
 
-if __name__=="__main__":
-    animacao()
+def teste_clipping():
+    window = Window(400,400)
+    viewport = [(200,200), (300,300)]
+
+    reta = [(1,1), (399,399)]
+    reta_desenhada = cohen_sutherland(200,200,300,300,1,1,399,399)
+    window.bresenham(reta_desenhada[0][0], reta_desenhada[0][1], reta_desenhada[1][0], reta_desenhada[1][1], (255, 255, 255))
+    while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    print("Comando: Cima")
+                elif event.key == pygame.K_DOWN:
+                    print("Comando: Baixo")
+                elif event.key == pygame.K_LEFT:
+                    print("Comando: Esquerda")
+                elif event.key == pygame.K_RIGHT:
+                    print("Comando: Direita")
+                elif event.key == pygame.K_SPACE:
+                    print("Comando: Espaço")  
         
+        pygame.display.update()
+
+def teste_clipping2():
+    window = Window(400,400)
+    viewport = [(200,200), (300,300)]
+    subject_polygon = [(250,250), (320,300), (280,300)]
+    clipping_polygon = [(200,200),(300,200),(300,300),(200,300)]
+    #reta = [(1,1), (399,399)]
+    subject_polygon = np.array(subject_polygon)
+    clipping_polygon = np.array(clipping_polygon)
+    clipped_polygon = clip(subject_polygon, clipping_polygon)
+    print(clipped_polygon)
+    clipped_polygon = [(int(x[0]), int(x[1])) for x in clipped_polygon]
+    p = Polygon(clipped_polygon)
+    p.texture = "./img/gato.jpg"
+    p.textureCoordenates=[[0,0],
+                          [1,0],
+                          [1,1],
+                          [0,1]]
+    #window.bresenham(reta_desenhada[0][0], reta_desenhada[0][1], reta_desenhada[1][0], reta_desenhada[1][1], (255, 255, 255))
+    while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    print("Comando: Cima")
+                elif event.key == pygame.K_DOWN:
+                    print("Comando: Baixo")
+                elif event.key == pygame.K_LEFT:
+                    print("Comando: Esquerda")
+                elif event.key == pygame.K_RIGHT:
+                    print("Comando: Direita")
+                elif event.key == pygame.K_SPACE:
+                    print("Comando: Espaço")  
+        p.scanlineT(window)
+        pygame.display.update()
+
+if __name__=="__main__":
+    #animacao()
+    teste_clipping2()
         
         
     
