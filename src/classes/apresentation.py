a4789d97a535254743504f85ae5aa0639b869b3a
Gabriel Magalhaes <magalhaesgabriel124@gmail.com>
2023-05-28 15:28:33 -0300

    apresentacao quase completa

--- src/classes/apresentation.py
+++ src/classes/apresentation.py
@@ -4,6 +4,8 @@ import pygame
 from random import randint
 from nave import Nave
 from estrela import estrela
+from Tranformations import translacao
+from copy import deepcopy
 
 def apresentation():
 
@@ -13,6 +15,77 @@ def apresentation():
     janela = Window(800,600)
     time_estrela=0
     time_planeta =0
+
+    M = Polygon([[140,80], #0
+                [160,80], #1
+                [170,110], #2
+                [180,80], #3
+                [200,80], #4
+                [200,140], #5
+                [180,140], #6
+                [180,100], #7
+                [170,130], #8
+                [160,100], #9
+                [160,140], #10
+                [140,140] #11
+                ])
+        
+    E = Polygon([[210,80],
+                [250,80],
+                [250,90],
+                [220,90],
+                [220,100],
+                [250,100],
+                [250,110],
+                [220,110],
+                [220,120],
+                [250,120],
+                [250,130],
+                [250,140],
+                [210,140]
+                ])
+    E2 = deepcopy(E)
+
+    translacao(E2,120,0)
+
+    T= Polygon([[260,80],
+                [320,80],
+                [320,100],
+                [300,100],
+                [300,140],
+                [280,140],
+                [280,100],
+                [260,100]
+                ])
+        
+        
+    R = Polygon([[450,80],
+                    [490,80],
+                    [490,140],
+                    [450,140]
+                    ])
+    
+    R.texture = "classes/R.png"
+
+    R.textureCoordenates = [[0,0],
+                            [1,0],
+                            [1,1],
+                            [0,1]]
+
+    S = Polygon([[580,80], #0
+                 [650,80],#1
+                 [650,100],#2
+                 [610,100],#3
+                 [610,110],#4
+                 [650,110],#5
+                 [650,140],#6
+                 [580,140],#7
+                 [580,130],#8
+                 [630,130],
+                 [630,120],
+                 [580,120]
+                 ])
+
     while(True):
         time_estrela+=1
         time_estrela = 0 if time_estrela>10 else time_estrela
@@ -90,39 +163,37 @@ def apresentation():
                                    [0,1]]
         start.scanlineT(janela)
 
-        # Letra O
-        """ xc,yc = 200,100
-
-        janela.drawEllipse(xc,yc,30,40,(255,255,255))
-        janela.floodFill(xc,yc,(255,255,255),(0,0,0))
-        janela.drawEllipse(xc,yc,20,30,(0,0,0))
-        janela.floodFill(xc,yc,(0,0,0),(255,255,255))
-
-        # Letra R
-
-        p = Polygon([[250,100],
-                     [300,100],
-                     [300,110],
-                     [260,110],
-                     [260,140],
-                     [250,140]
-                     ])
-        p.scanline(janela,(255,255,255))
-
-        # Letra b
-
-        p = Polygon([[320,70],
-                     [330,70],
-                     [330,105],
-                     [360,105],
-                     [360,140],
-                     [320,140]
-                     ])
+        fundo_titulo = Polygon([[100,60],
+                                [700,60],
+                                [700,160],
+                                [100,160]])
         
-        p.scanline(janela,(255,255,255))
+        fundo_titulo.scanline(janela,(0,0,0))
         
-        p= Nave(390,120,3)
-        p.scanline(janela,(255,255,255)) """
+        
+        R.scanlineT(janela)
+
+        E.scanline(janela,(255,255,255))
+        
+        T.scanline(janela,(255,255,255))
+
+        E2.scanline(janela,(255,255,255))
+
+        janela.drawEllipse(410,110,25,30,(255,255,255))
+        janela.floodFill(410,110,(255,255,255),(0,0,0))
+
+        janela.drawEllipse(410,110,10,15,(0,0,0))
+        janela.floodFill(410,110,(0,0,0),(255,255,255))
+
+        M.scanline(janela,(255,255,255))
+
+        janela.drawEllipse(410+120,110,25,30,(255,255,255))
+        janela.floodFill(410+120,110,(255,255,255),(0,0,0))
+
+        janela.drawEllipse(410+120,110,10,15,(0,0,0))
+        janela.floodFill(410+120,110,(0,0,0),(255,255,255))
+
+        S.scanline(janela ,(255,255,255))
         
 
         pygame.display.update()
